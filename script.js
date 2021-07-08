811ade4d128636f5e67ef29aeaa2bba662f41c3c
Ashley Manese <ashleymanese31@gmail.com>
2021-07-08 14:51:24 -0700

    revised from code review

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 let img;
 let backgroundImg;
 function preload() {
-  img = loadImage('./images/turtle.svg');
-  backgroundImg = loadImage('./images/beach.svg')
+  img = loadImage('./images/turtleImg.svg');
+  backgroundImg = loadImage('./images/beachImg.svg')
 }
 
 function setup() {
   createCanvas(displayWidth, displayHeight);
-  backgroundImg.resize(windowHeight,windowWidth - 200)
+  backgroundImg.resize(windowWidth, windowHeight)
   background(backgroundImg)
   noStroke()
  
-  
 }
 
 function draw() {
   img.resize(200,200);
-  cursor(image(img, mouseX-20,mouseY-20))
+  image(img, mouseX-20,mouseY-20)
   frameRate(3)
   
   fill('#C6B6A8')
@@ -24,21 +23,14 @@ function draw() {
   ellipse(mouseX,mouseY,25,50)
   frameRate(2.5)
 
-  fill('#C6B6A8')
   rotate(.1)
   ellipse(mouseX-50,mouseY-100,25,50)
-  frameRate(2.5)
   
 
- 
-    
-  
-  
+  console.log(mouseClicked)
+}
 
-  
-  
-
-  
+function mouseClicked(){
   
  
 }
